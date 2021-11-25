106b8f4e0bf26a20471a37b9302a4115ca944f31
Mateus <cuelassis@gmail.com>
2021-11-25 17:09:04 -0300

    Testes valida cpf e valida nome de usuario

--- Atividade1/Usuario.cs
+++ Atividade1/Usuario.cs
@@ -18,12 +18,15 @@ namespace Atividade1
 
         public Usuario(string nome, string cpf)
         {
-            if (string.IsNullOrWhiteSpace(nome))
-               throw new Exception();
+            if (string.IsNullOrEmpty(nome))
+               throw new Exception("Nome nao pode ser vazio");
+            if (string.IsNullOrEmpty(cpf))
+                throw new Exception("Cpf nao pode ser vazio");
             this.nome = nome;
             this.cpf = cpf;
             id += 1;
         }
+        
 
         public float Id
         {
@@ -35,6 +38,67 @@ namespace Atividade1
 
 
         public string Cpf { get { return cpf; } set { cpf = value; } }
+
+        public bool validaCpf(string cpf) {
+
+            string valor = cpf.Replace(".", "");
+            valor = valor.Replace("-", "");
+
+            if (valor.Length != 11)
+            {
+                return false;
+            }
+           
+            bool igual = true;
+
+            for (int i = 1; i < 11 && igual; i++)
+            {
+                if (valor[i] != valor[0])
+                {
+                    igual = false;
+                }
+            }
+
+            if (igual || valor == "12345678909")
+                return false;
+
+            int[] numeros = new int[11];
+
+            for (int i = 0; i < 11; i++)
+                numeros[i] = int.Parse(valor[i].ToString());
+
+            int soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += (10 - i) * numeros[i];
+
+            int resultado = soma % 11;
+
+            if (resultado == 1 || resultado == 0)
+            {
+                if (numeros[9] != 0)
+                    return false;
+            }
+            else if (numeros[9] != 11 - resultado)
+                return false;
+            
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+                soma += (11 - i) * numeros[i];
+
+            resultado = soma % 11;
+
+            if (resultado == 1 || resultado == 0)
+            {
+                if (numeros[10] != 0)
+                    return false;
+            }
+            else
+                if (numeros[10] != 11 - resultado)
+                return false;
+
+            return true;
+        }
         
         
     }
--- Testes1/TestesUsuarios.cs
+++ Testes1/TestesUsuarios.cs
@@ -10,6 +10,7 @@ namespace Testes1
 {
     public class TestesUsuarios
     {
+
         [Fact]
         public void TestaUsuario() {
             // Definindo cenario: Arrange
@@ -25,12 +26,40 @@ namespace Testes1
 
         [Fact]
         public void TestaNomeVazio() {
+            
             string nome = "";
             string cpf = "41971654809";
+            Usuario usuario2;
 
-            Usuario usuario2 = new Usuario(nome, cpf);
-
-            Assert.NotEmpty(usuario2.Nome);
+            var execaoObtida = Assert.Throws<Exception>(() => usuario2 = new Usuario(nome, cpf));
+            var execaoEsperada = "Nome nao pode ser vazio";
+            Assert.Equal(execaoEsperada, execaoObtida.Message);
         }
+        
+        [Fact]
+        public void TestaCpfVazio()
+        {
+
+            string nome = "Mateus Cuel Assis";
+            string cpf = "";
+            Usuario usuario3;
+
+            var execaoObtida = Assert.Throws<Exception>(() => usuario3 = new Usuario(nome, cpf));
+            var execaoEsperada = "Cpf nao pode ser vazio";
+            Assert.Equal(execaoEsperada, execaoObtida.Message);
+        }
+
+        [Fact]
+        public void ValidaCpf() {
+
+            string nome = "mateus cuel asis";
+            string cpf = "777.777.777-77";
+            Usuario usuario = new Usuario(nome,cpf);
+
+            bool valorEsperado = false;
+            Assert.Equal(valorEsperado,usuario.validaCpf(cpf));
+        }
+
+
     }
 }
